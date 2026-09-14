9e7b0a3094ef4c514dd4a1e4140c4be700245e59
bakkeby <bakkeby@gmail.com>
2026-09-14 09:41:40 +0200

    Bump to 0785a28.
    fix out-of-bounds read of the Xinerama screen array with no screens
    
    This can happen if dmenu is run with Xinerama enabled and no screens are
    detected.
    
    Also fallback to the first screen if there is no intersection of the root
    pointer to the screens found.
    
    Reported by: jb19357 <jb19357@protonmail.com>
    Who used Claude to write a report.
    
    I cannot reproduce this issue, but it seems logical.
    
    Ref.
    https://git.suckless.org/dmenu/commit/0785a28f1d6e27e18ab998d20b0390e2c3ec226c.html

--- dmenu.c
+++ dmenu.c
@@ -1780,6 +1780,8 @@ setup(void)
 #ifdef XINERAMA
 	i = 0;
 	if (parentwin == root && (info = XineramaQueryScreens(dpy, &n))) {
+		if (n < 1)
+			die("Xinerama reported no screens");
 		XGetInputFocus(dpy, &w, &di);
 		if (mon >= 0 && mon < n)
 			i = mon;
@@ -1802,6 +1804,9 @@ setup(void)
 			for (i = 0; i < n; i++)
 				if (INTERSECT(x, y, 1, 1, info[i]) != 0)
 					break;
+		/* fallback to the first screen if there is no intersection */
+		if (i >= n)
+			i = 0;
 
 		#if CENTER_PATCH
 		if (center) {
